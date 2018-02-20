7dabf5d1be60ea599e821b2a7b779d0864a396e5
VirtualRavenn <32814604+VirtualRavenn@users.noreply.github.com>
2018-02-20 14:04:36 +1100

    Create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+client.on("message", message => {
+  if (message.author.id === "414341347458416641") message.reply("you love brianna");
+});
+
+client.login(process.env.BOT_TOKEN);
