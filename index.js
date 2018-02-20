e464f875cb73749e42d6209c996f7de96893af85
VirtualRavenn <32814604+VirtualRavenn@users.noreply.github.com>
2018-02-21 09:42:52 +1100

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const client = new Discord.Client();
 const prefix = "#";
 
 client.on("message", message => {
-  if (message.author.id === "292545042059624449") message.reply("You dun fucked up boy");
+  if (message.author.id === "292545042059624449") message.reply("You dun fucked up dawg");
 });
                  
 client.login(process.env.BOT_TOKEN);
