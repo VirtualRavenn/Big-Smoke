c5ee68545d5e5b4a6f6062df88935f38045efb27
VirtualRavenn <32814604+VirtualRavenn@users.noreply.github.com>
2018-02-20 19:20:24 +1100

    Update index.js

--- index.js
+++ index.js
@@ -24,6 +24,5 @@ if(command === "#kick") {
   await member.kick(reason)
   .catch(error => message.reply('Sorry ${message.author} I couldn't kick because of : ${error}));
    message.reply(${member.user.tag} has been kicked by ${message.author.tag} because: ${reason}'
-                 });
-            
+});         
 client.login(process.env.BOT_TOKEN);
