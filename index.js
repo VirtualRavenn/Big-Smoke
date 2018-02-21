fc91117460087788c184f891a801cfe33f9d1561
VirtualRavenn <32814604+VirtualRavenn@users.noreply.github.com>
2018-02-21 13:36:48 +1100

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ bot.on("message". function(message) {
 });
 
 client.on("message", message => {
-  if (message.author.id === "292545042059624449") message.reply("You dun fucked up dawg");
+  if (message.author.id === "414341347458416641") message.reply("You dun fucked up fool");
 });
                  
 client.login(process.env.BOT_TOKEN);
