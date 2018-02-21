6699d72754c4c02c75df3599cb4fd34b1bfde1a4
VirtualRavenn <32814604+VirtualRavenn@users.noreply.github.com>
2018-02-21 13:49:47 +1100

    Update index.js

--- index.js
+++ index.js
@@ -2,27 +2,7 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 
 client.on("message", message => {
-  if (message.author.id === "414341347458416641") message.reply("You dun fucked up fool");
-});
-
-client.on("message", message => {
-  if (message.author.id === "414341347458416641") message.reply("You ugly as fuck fool");
-});
-
-client.on("message", message => {
-  if (message.author.id === "414341347458416641") message.reply("Why the fuck you here fool");
-});
-
-client.on("message", message => {
-  if (message.author.id === "414341347458416641") message.reply("You stupid as fuck you dummis");
-});
-
-client.on("message", message => {
-  if (message.author.id === "414341347458416641") message.reply("Come fight me you fucking flop");
-});
-
-client.on("message", message => {
-  if (message.author.id === "414341347458416641") message.reply("You look like shrek fool");
+  if (message.author.id === "414341347458416641") message.reply("All you had to do was follow the **damn** train");
 });
                  
 client.login(process.env.BOT_TOKEN);
