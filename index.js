2cfc534b4ad120a626a384c24a604a0a7a5f76c2
VirtualRavenn <32814604+VirtualRavenn@users.noreply.github.com>
2018-02-21 13:38:19 +1100

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,6 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
-bot.on("message". function(message) {
-       if (message.author.equals(bot.user)) return;
-
-       if (message.content == "hello") {
-         message.channel.sendmessage("Hi, There");
-       }
-});
-
 client.on("message", message => {
   if (message.author.id === "414341347458416641") message.reply("You dun fucked up fool");
 });
