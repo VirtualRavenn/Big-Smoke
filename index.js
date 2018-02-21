717d86915a250949e3b63b5c27f23ed0d148d514
VirtualRavenn <32814604+VirtualRavenn@users.noreply.github.com>
2018-02-21 14:31:43 +1100

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 
 client.on("message", message => {
-  if (message.author.id === "415704844221808640") message.reply("All you had to do was follow the **Damn** Train!");
+  if (message.author.id === "292545042059624449") message.reply("**Fool** you ank suppose to be in here.");
 });
 
 client.on('message', msg => {
