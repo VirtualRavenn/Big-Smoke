1dda541b87667833486f377455d5fefda8925558
VirtualRavenn <32814604+VirtualRavenn@users.noreply.github.com>
2018-02-22 09:57:47 +1100

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ client.on("message", message => {
   if (message.author.id === "415704844221808640") message.reply("**Fool** you aren't suppose to be in here.");
 });
 
+client.on("message", message => {
+  if (message.author.id === "415996653279903744") message.reply("**Fool** you aren't suppose to be in here.");
+});
+
 client.on('message', msg => {
   if (msg.content.toLowerCase()=== "what's ya order?") {
     msg.reply("I'll have 2 number nines, a number nine large, a number six with extra dip, a number seven, 2 number 45's, one with cheese and a large soda.");
