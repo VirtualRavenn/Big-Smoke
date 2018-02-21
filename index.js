4e56e0cc4c8c8cee67b42373a018bfe624fa5494
VirtualRavenn <32814604+VirtualRavenn@users.noreply.github.com>
2018-02-21 14:16:49 +1100

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
-client.on("message", message => {
-  if (message.author.id === "414341347458416641") message.reply("All you had to do was follow the **damn** train");
+client.on('message', msg => {
+  if (msg.content.toLowerCase()=== "what's ya order?") {
+    msg.reply("I'll have 2 number nines, a number nine large, a number six with extra dip, a number seven, 2 number 45's, one with cheese and a large soda.");
+  }
 });
-                 
+
 client.login(process.env.BOT_TOKEN);
