cd8dbe2f126e6ea56dd423f3ff900cf105c66ffe
VirtualRavenn <32814604+VirtualRavenn@users.noreply.github.com>
2018-02-21 11:28:52 +1100

    Update index.js

--- index.js
+++ index.js
@@ -1,57 +1,5 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-const fs = require("fs");
-
-const config = require("./config.json");
-
-fs.readdir("./events/", (err, files) => {
-  if (err) return console.error(err);
-  files.forEach(file => {
-    let eventFunction = require(`./events/${file}`);
-    let eventName = file.split(".")[0];
-    client.on(eventName, (...args) => eventFunction.run(client, ...args));
-  });
-});
-
-client.on("message", message => {
-  if (message.author.bot) return;
-  if(message.content.indexOf(config.prefix) !== 0) return;
-  
-   const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
-  const command = args.shift().toLowerCase();
-  
-   try {
-    let commandFile = require(`./commands/${command}.js`);
-    commandFile.run(client, message, args);
-  } catch (err) {
-    console.error(err);
-  }
-});
-
-exports.run = (client, message, args) => {
-    message.channel.send("pong!").catch(console.error);
-}
-
-exports.run = (client, message, [mention, ...reason]) => {
-  const modRole = message.guild.roles.find("name", "Mods");
-  if (!modRole)
-    return console.log("The Mods role does not exist");
-
-  if (!message.member.roles.has(modRole.id))
-    return message.reply("You can't use this command.");
-
-  if (message.mentions.members.size === 0)
-    return message.reply("Please mention a user to kick");
-
-  if (!message.guild.me.hasPermission("KICK_MEMBERS"))
-    return message.reply("");
-
-  const kickMember = message.mentions.members.first();
-
-  kickMember.kick(reason.join(" ")).then(member => {
-    message.reply(`${member.user.username} was succesfully kicked.`);
-  });
-};
 
 client.on("message", message => {
   if (message.author.id === "292545042059624449") message.reply("You dun fucked up dawg");
