0dcdd0bcad746240147070e85d24c2256e747288
VirtualRavenn <32814604+VirtualRavenn@users.noreply.github.com>
2018-02-21 13:45:53 +1100

    Update index.js

--- index.js
+++ index.js
@@ -8,20 +8,21 @@ client.on("message", message => {
 client.on("message", message => {
   if (message.author.id === "414341347458416641") message.reply("You ugly as fuck fool");
 });
+
+client.on("message", message => {
+  if (message.author.id === "414341347458416641") message.reply("Why the fuck you here fool");
+});
+
+client.on("message", message => {
+  if (message.author.id === "414341347458416641") message.reply("You stupid as fuck you dummis");
+});
+
+client.on("message", message => {
+  if (message.author.id === "414341347458416641") message.reply("Come fight me you fucking flop");
+});
+
+client.on("message", message => {
+  if (message.author.id === "414341347458416641") message.reply("You look like shrek fool");
+});
                  
 client.login(process.env.BOT_TOKEN);
-
-ewrfvcqwgefbhesbhja4eghdr4eff
-
-efef
-fef
-e
-efe
-f
-e
-f
-e
-f
-e
-f
-e
